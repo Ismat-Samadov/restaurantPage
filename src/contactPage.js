bad2e0d73f6c2f9ba1947dd4ca18545de1da216c
Ismat-Samadov <ismetsemedov@gmail.com>
2024-02-10 18:27:29 +0400

    check

--- src/contactPage.js
+++
@@ -1,7 +0,0 @@
-export function loadContactPage() {
-    const content = document.getElementById('content');
-    const contactDiv = document.createElement('div');
-    contactDiv.textContent = "Contact us at: example@example.com";
-    // Add more contact information here as needed
-    content.appendChild(contactDiv);
-}
--- src/menuPage.js
+++
@@ -1,6 +0,0 @@
-export function loadMenuPage() {
-    const content = document.getElementById('content');
-    const menuDiv = document.createElement('div');
-    menuDiv.textContent = "Check out our menu:";
-    content.appendChild(menuDiv);
-}
